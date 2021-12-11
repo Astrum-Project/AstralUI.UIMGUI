3d9d218747fe2deee8bed76fc5dfbaba97180fa1
xKiraiChan <xKiraiChan@gmail.com>
2021-12-10 19:51:33 -0500

    c# 10, add menu color melonpref

--- AstralUI.UIMGUI.cs
+++ AstralUI.UIMGUI.cs
@@ -5,21 +5,38 @@ using UnityEngine;
 using static Astrum.AstralCore.Managers.CommandManager;
 using static Astrum.AstralCore.Managers.ModuleManager;
 
-[assembly: MelonInfo(typeof(Astrum.AstralUI.UIMGUI), "AstralUI.UIMGUI", "0.1.0", downloadLink: "github.com/Astrum-Project/AstralUI.UIMGUI")]
+[assembly: MelonInfo(typeof(Astrum.AstralUI.UIMGUI), "AstralUI.UIMGUI", "0.2.0", downloadLink: "github.com/Astrum-Project/AstralUI.UIMGUI")]
 [assembly: MelonColor(ConsoleColor.DarkMagenta)]
 
 namespace Astrum.AstralUI
 {
     public class UIMGUI : MelonMod
     {
-        public Module currentModule = null;
-        private Rect position = new Rect(Screen.width / 2 - 78, Screen.height / 2 - 307, 156, 482);
+        public static Color32 color = new(0x56, 0x00, 0xA5, 0xFF);
+        public static Module currentModule = null;
+
+        private static Rect position = new(Screen.width / 2 - 78, Screen.height / 2 - 307, 156, 482);
+
+        public override void OnApplicationStart()
+        {
+            MelonPreferences_Category category = MelonPreferences.CreateCategory("Astrum-AstralUI-UIMGUI", "AstralUI UIMGUI");
+
+            category.CreateEntry(nameof(color), new Color32(0x56, 0x00, 0xA5, 0xFF), "Color");
+        }
+
+        public override void OnPreferencesSaved() => OnPreferencesLoaded();
+        public override void OnPreferencesLoaded()
+        {
+            MelonPreferences_Category category = MelonPreferences.GetCategory("Astrum-AstralUI-UIMGUI");
+
+            color = category.GetEntry<Color32>(nameof(color)).Value;
+        }
 
         public override void OnGUI()
         {
             if (!Input.GetKey(KeyCode.Tab)) return;
 
-            GUI.backgroundColor = new Color32(0x56, 0x00, 0xA5, 0xFF);
+            GUI.backgroundColor = color;
             GUI.color = Color.white;
 
             position = GUI.Window(25, position, (GUI.WindowFunction)ModulesWindow, "Modules");
@@ -44,28 +61,22 @@ namespace Astrum.AstralUI
 
             foreach (KeyValuePair<string, Command> command in currentModule.commands)
             {
-                if (command.Value is ConVar<bool>)
+                if (command.Value is ConVar<bool> cb) 
+                    cb.Value = GUI.Toggle(CreateRect(ref i), cb.Value, command.Key);
+                else if (command.Value is ConVar<float> cf)
                 {
-                    ConVar<bool> c = (ConVar<bool>)command.Value;
-                    c.Value = GUI.Toggle(CreateRect(ref i), c.Value, command.Key);
-                } 
-                else if (command.Value is ConVar<float>)
-                {
-                    ConVar<float> c = (ConVar<float>)command.Value;
-                    GUI.Label(CreateRect(ref i), command.Key + $": ({c.Value.ToString("0.00")})");
-                    c.Value = GUI.HorizontalSlider(CreateRect(ref i), c.Value, c.Value - 10, c.Value + 10);
+                    GUI.Label(CreateRect(ref i), command.Key + $": ({cf.Value:0.00})");
+                    cf.Value = GUI.HorizontalSlider(CreateRect(ref i), cf.Value, cf.Value - 10, cf.Value + 10);
                 }
-                else if (command.Value is Button) {
+                else if (command.Value is Button)
+                {
                     if (GUI.Button(CreateRect(ref i), command.Key))
                         (command.Value as Button)?.onClick();
                 }
-                else
-                {
-                    GUI.Label(CreateRect(ref i), command.Key + " (Unsupported type)");
-                }
+                else GUI.Label(CreateRect(ref i), command.Key + " (Unsupported type)");
             }
         }
 
-        private static Rect CreateRect(ref int i) => new Rect(3, i++ * 22 + 21, 300, 20);
+        private static Rect CreateRect(ref int i) => new(3, i++ * 22 + 21, 300, 20);
     }
 }
