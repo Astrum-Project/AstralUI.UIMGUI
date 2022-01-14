952440134b7fa13451ca83abed7ad9506d71423d
xKiraiChan <xKiraiChan@gmail.com>
2022-01-13 22:07:35 -0500

    use guilayout

--- AstralUI.UIMGUI.cs
+++ AstralUI.UIMGUI.cs
@@ -5,17 +5,21 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-[assembly: MelonInfo(typeof(Astrum.AstralUI.UIMGUI), "AstralUI.UIMGUI", "1.0.0", downloadLink: "github.com/Astrum-Project/AstralUI.UIMGUI")]
+[assembly: MelonInfo(typeof(Astrum.AstralUI.UIMGUI), "AstralUI.UIMGUI", "1.1.0", downloadLink: "github.com/Astrum-Project/AstralUI.UIMGUI")]
 [assembly: MelonColor(ConsoleColor.DarkMagenta)]
 
 namespace Astrum.AstralUI
 {
     public class UIMGUI : MelonMod
     {
+        private static readonly UnhollowerBaseLib.Il2CppReferenceArray<GUILayoutOption> options = new(0);
+
         public static Color32 color = new(0x56, 0x00, 0xA5, 0xFF);
         public static Module currentModule = null;
 
         private static Rect position = new(Screen.width / 2 - 78, Screen.height / 2 - 307, 156, 482);
+        private static Vector2 scrollModules = default;
+        private static Vector2 scrollCommands = default;
 
         public override void OnApplicationStart()
         {
@@ -46,37 +50,39 @@ namespace Astrum.AstralUI
 
         private void ModulesWindow(int id)
         {
-            int i = 0;
+            scrollModules = GUILayout.BeginScrollView(scrollModules, options);
 
             foreach (KeyValuePair<string, Module> module in CoreUI.Modules)
-                if (GUI.Button(new Rect(3, i++ * 22 + 21, 150, 20), module.Key))
+                if (GUILayout.Button(module.Key, options))
                     currentModule = module.Value;
+            
+            GUILayout.EndScrollView(true);
 
             GUI.DragWindow(new Rect(0, 0, 156, 20));
         }
 
         private void CommandsWindow(int windowID)
         {
-            int i = 0;
+            scrollCommands = GUILayout.BeginScrollView(scrollCommands, options);
 
             foreach (KeyValuePair<string, UIBase> command in currentModule.Commands)
             {
                 if (command.Value is UIFieldProp<bool> cb) 
-                    cb.Value = GUI.Toggle(CreateRect(ref i), cb.Value, command.Key);
+                    cb.Value = GUILayout.Toggle(cb.Value, command.Key, options);
                 else if (command.Value is UIFieldProp<float> cf)
                 {
-                    GUI.Label(CreateRect(ref i), command.Key + $": ({cf.Value:0.00})");
-                    cf.Value = GUI.HorizontalSlider(CreateRect(ref i), cf.Value, cf.Value - 10, cf.Value + 10);
+                    GUILayout.Label(command.Key + $": ({cf.Value:0.00})", options);
+                    cf.Value = GUILayout.HorizontalSlider(cf.Value, cf.Value - 10, cf.Value + 10, options);
                 }
                 else if (command.Value is UIButton)
                 {
-                    if (GUI.Button(CreateRect(ref i), command.Key))
+                    if (GUILayout.Button(command.Key, options))
                         (command.Value as UIButton)?.Click();
                 }
-                else GUI.Label(CreateRect(ref i), command.Key + " (Unsupported type)");
+                else GUILayout.Label(command.Key + " (Unsupported type)", options);
             }
-        }
 
-        private static Rect CreateRect(ref int i) => new(3, i++ * 22 + 21, 300, 20);
+            GUILayout.EndScrollView(true);
+        }
     }
 }
